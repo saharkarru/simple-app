76c9ecb533daceaf0fd36d79a825907a7eca08e7
saharkarru <79689411+saharkarru@users.noreply.github.com>
2021-04-28 18:56:20 +0530

    Update GreetingController.java

--- src/main/java/com/okta/developer/simpleapp/controller/GreetingController.java
+++ src/main/java/com/okta/developer/simpleapp/controller/GreetingController.java
@@ -4,7 +4,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerpublic
+@RestController
 public class GreetingController {
 	@GetMapping("/greeting")
 	public String greet(@AuthenticationPrincipal OidcUser user){
